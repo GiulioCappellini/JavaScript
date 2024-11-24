0cae2ffb8113c8809919fa365c2e05c94acde4f8
GiulioCappellini <giuliodevcappellini@gmail.com>
2024-11-24 02:59:12 -0300

    criei o pratica2

--- pratica/scripts/pratics2.js
+++ pratica/scripts/pratics2.js
@@ -14,10 +14,29 @@ function verificar(event) {
     if (age <= 0 || age > 110) {
         window.alert("Digite um ano válido!")   //Verifica se o ano inserido é valido para proseguir
         return false;
-    } else if (age == 1) {agetext = "ano"}
+    } else if (age == 1) {
+        agetext = "ano"
+        if (sex === "Homem") {sex = "Menino"} else {sex = "Menina"}
+    }
 
     let artigo = "uma"
-    if (sex == "Homem") {artigo = "um"}
-    const divresult = form.querySelector("#result")
-    divresult.innerHTML=`Detectamos ${artigo} ${sex} de ${age} ${agetext}!`;
+    if (sex == "Homem" || sex == "Menino") {artigo = "um"}
+    const presult = form.querySelector("#presult")
+    presult.innerHTML=`Detectamos ${artigo} ${sex} de ${age} ${agetext}!`;
+
+    const resultimg = form.querySelector("#divresult")
+    resultimg.classList.add("result")
+    if (age == 1) {
+        if (sex === "Menina") {resultimg.style.backgroundImage = "url('imagens/imagens_pratica2/bebe-menina.jpg')";}
+        else {resultimg.style.backgroundImage = "url('imagens/imagens_pratica2/bebe-menino.jpg')";}
+    } else if (age < 12) {
+        if (sex === "Menina") {resultimg.style.backgroundImage = "url('imagens/imagens_pratica2/menina.jpg')";}
+        else {resultimg.style.backgroundImage = "url('imagens/imagens_pratica2/menino.jpg')";}
+    } else if (age < 60) {
+        if (sex === "Mulher") {resultimg.style.backgroundImage = "url('imagens/imagens_pratica2/mulher.jpg')";}
+        else {resultimg.style.backgroundImage = "url('imagens/imagens_pratica2/homem.jpg')";}
+    } else {
+        if (sex === "Mulher") {resultimg.style.backgroundImage = "url('imagens/imagens_pratica2/idosa.jpg')";}
+        else {resultimg.style.backgroundImage = "url('imagens/imagens_pratica2/idoso.jpg')";}
+    }
 };
